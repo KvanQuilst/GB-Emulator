6b0fa9722845bb71a90115c89db98d159e21df78
KvanQuilst <eskew.dylan@outlook.com>
2021-09-15 13:33:11 -0700

    incremental update

--- registers.h
+++ registers.h
@@ -4,12 +4,16 @@
  * struct representation of gb registers, compatible with register
  * pairing, allowing for 16-bit registers
  * 
- * registers a-l are all 8bit registers; the stack pointer and program counter are 16bit
+ * registers a-l are all 8bit registers; the stack pointer
+ * and program counter are 16bit
+ * 
+ * Note: Individual registers are reversed because it is
+ * (assumingly) running on a little-endian machine
  * 
  *   -----------
- *  |  A  |  F  | <--- F is indirectly accessible; stores results of various math ops as well as op flags
- *  |-----------|
- *  |  B  |  C  |
+ *  |  A  |  F  | <--- F is indirectly accessible; 
+ *  |-----------|	   stores results of various math 
+ *  |  B  |  C  |	   ops as well as op flags
  *  |-----------|
  *  |  D  |  E  |
  *  |-----------|
@@ -20,6 +24,7 @@
  *  |     PC    | <-- Program Counter (16bit)
  *   -----------
  */
+#pragma once
 
 #include <stdint.h>
 
@@ -27,8 +32,8 @@ struct registers {
     struct {
         union {
             struct {
-                uint8_t a;
                 uint8_t f;
+                uint8_t a;
             };
             uint16_t af;
         };
@@ -36,8 +41,8 @@ struct registers {
     struct {
         union {
             struct {
-                uint8_t b;
                 uint8_t c;
+                uint8_t b;
             };
             uint16_t bc;
         };
@@ -45,8 +50,8 @@ struct registers {
     struct {
         union {
             struct {
-                uint8_t d;
                 uint8_t e;
+                uint8_t d;
             };
             uint16_t de;
         };
@@ -54,13 +59,12 @@ struct registers {
     struct {
         union {
             struct {
-                uint8_t h;
                 uint8_t l;
+                uint8_t h;
             };
             uint16_t hl;
         };
     };
     uint16_t sp;
     uint16_t pc;
-};
-extern registers;
+} extern registers;
