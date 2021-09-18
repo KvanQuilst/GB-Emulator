ae1b889430bfa4f31fc335419f86f161267c24ea
KvanQuilst <eskew.dylan@outlook.com>
2021-09-17 23:37:09 -0700

    added all the n-reg loads; added all disas to instr arr

--- cpu.c
+++ cpu.c
@@ -10,19 +10,9 @@
 
 #include "cpu.h"
 
-// struct containing important information regarding 
-// the processor instructions for GameBoy
-struct instruction 
-{
-    char *disas;        // instruction disassembly
-    uint8_t ops;        // number of operands (0-2)
-    void *exec;  // function to execute instruction
-    uint8_t ticks;      // machine time needed
-};
-
 const struct instruction instr[256] =
 {
-    {"nop", 0, nop, 1},					// 0x00
+    {"nop",	0, nop, 1},					// 0x00
     {"LD BC,0x%04x", 2, ld_bc_nn, 3},	// 0x01 
     {"LD (BC),A", 0, undefined, 2},		// 0x02
     {"INC BC", 0, inc_bc, 2},			// 0x03
@@ -84,195 +74,200 @@ const struct instruction instr[256] =
     {"DEC SP", 0, dec_sp, 2},			// 0x3B
     {"INC A", 0, inc_a, 1},				// 0x3C
     {"DEC A", 0, dec_a, 1},				// 0x3D
-    {"LD A,0x%02x", 0, undefined, 2},	// 0x3E
+    {"LD A,0x%02x", 0, ld_a_n, 2},		// 0x3E
     {"CCF", 0, undefined, 1},			// 0x3F
     {"LD B,B", 0, nop, 1},				// 0x40
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
-    {"nop", 0, undefined, 1},
+    {"LD B,C", 0, ld_b_c, 1},			// 0x41
+    {"LD B,D", 0, ld_b_d, 1},			// 0x42
+    {"LD B,E", 0, ld_b_e, 1},			// 0x43
+    {"LD B,H", 0, ld_b_h, 1},			// 0x44
+    {"LD B,L", 0, ld_b_l, 1},			// 0x45
+    {"LD B,(HL)", 0, undefined, 2},		// 0x46
+    {"LD B,A", 0, ld_b_a, 1},			// 0x47
+    {"LD C,B", 0, ld_c_b, 1},			// 0x48
+    {"LD C,C", 0, nop, 1},				// 0x49
+    {"LD C,D", 1, ld_c_d, 1},			// 0x4A
+    {"LD C,E", 0, ld_c_e, 1},			// 0x4B
+    {"LD C,H", 0, ld_c_h, 1},			// 0x4C
+    {"LD C,L", 0, ld_c_l, 1},			// 0x4D
+    {"LD C,(HL)", 0, undefined, 2},		// 0x4E
+    {"LD C,A", 0, ld_c_a, 1},			// 0x4F
+    {"LD D,B", 0, ld_d_b, 1},			// 0x50
+    {"LD D,C", 0, ld_d_c, 1},			// 0x51
+    {"LD D,D", 0, nop, 1},				// 0x52
+    {"LD D,E", 0, ld_d_e, 1},			// 0x53
+    {"LD D,H", 0, ld_d_h, 1},			// 0x54
+    {"LD D,L", 0, ld_d_l, 1},			// 0x55
+    {"LD D,(HL)", 0, undefined, 2},		// 0x56
+    {"LD D,A", 0, ld_d_a, 1},			// 0x57
+    {"LD E,B", 0, ld_e_b, 1},			// 0x58
+    {"LD E,C", 0, ld_e_c, 1},			// 0x59
+    {"LD E,D", 0, ld_e_d, 1},			// 0x5A
+    {"LD E,E", 0, nop, 1},				// 0x5B
+    {"LD E,H", 0, ld_e_h, 1},			// 0x5C
+    {"LD E,L", 0, ld_e_l, 1},			// 0x5D
+    {"LD E,(HL)", 0, undefined, 2},		// 0x5E
+    {"LD E,A", 0, ld_e_a, 1},			// 0x5F
+    {"LD H,B", 0, ld_h_b, 1},			// 0x60
+    {"LD H,C", 0, ld_h_c, 1},			// 0x61
+    {"LD H,D", 0, ld_h_d, 1},			// 0x62
+    {"LD H,E", 0, ld_h_e, 1},			// 0x63
+    {"LD H,H", 0, nop, 1},				// 0x64
+    {"LD H,L", 0, ld_h_l, 1},			// 0x65
+    {"LD H,(HL)", 0, undefined, 2},		// 0x66
+    {"LD H,A", 0, ld_h_a, 1},			// 0x67
+    {"LD L,B", 0, ld_l_b, 1},			// 0x68
+    {"LD L,C", 0, ld_l_c, 1},			// 0x69
+    {"LD L,D", 0, ld_l_d, 1},			// 0x6A
+    {"LD L,E", 0, ld_l_e, 1},			// 0x6B
+    {"LD L,H", 0, ld_l_h, 1},			// 0x6C
+    {"LD L,L", 0, nop, 1},				// 0x6D
+    {"LD L,(HL)", 0, undefined, 2},		// 0x6E
+    {"LD L,A", 0, ld_l_a, 1},			// 0x6F
+    {"LD (HL),B", 0, undefined, 2},		// 0x70
+    {"LD (HL),C", 0, undefined, 2},		// 0x71
+    {"LD (HL),D", 0, undefined, 2},		// 0x72
+    {"LD (HL),E", 0, undefined, 2},		// 0x73
+    {"LD (HL),H", 0, undefined, 2},		// 0x74
+    {"LD (HL),L", 0, undefined, 2},		// 0x75
+    {"HALT", 0, undefined, 1},			// 0x76
+    {"LD (HL),A", 0, undefined, 2},		// 0x77
+    {"LD A,B", 0, ld_a_b, 1},			// 0x78
+    {"LD A,C", 0, ld_a_c, 1},			// 0x79
+    {"LD A,D", 0, ld_a_d, 1},			// 0x7A
+    {"LD A,E", 0, ld_a_e, 1},			// 0x7B
+    {"LD A,H", 0, ld_a_h, 1},			// 0x7C
+    {"LD A,L", 0, ld_a_l, 1},			// 0x7D
+    {"LD A,(HL)", 0, undefined, 2},		// 0x7E
+    {"LD A,A", 0, nop, 1},				// 0x7F
+    {"ADD A,B", 0, undefined, 1},		// 0x80
+    {"ADD A,C", 0, undefined, 1},		// 0x81
+    {"ADD A,D", 0, undefined, 1},		// 0x82
+    {"ADD A,E", 0, undefined, 1},		// 0x83
+    {"ADD A,H", 0, undefined, 1},		// 0x84
+    {"ADD A,L", 0, undefined, 1},		// 0x85
+    {"ADD A,(HL)", 0, undefined, 2},	// 0x86
+    {"ADD A,A", 0, undefined, 1},		// 0x87
+    {"ADC A,B", 0, undefined, 1},		// 0x88
+    {"ADC A,C", 0, undefined, 1},		// 0x89
+    {"ADC A,D", 0, undefined, 1},		// 0x8A
+    {"ADC A,E", 0, undefined, 1},		// 0x8B
+    {"ADC A,H", 0, undefined, 1},		// 0x8C
+    {"ADC A,L", 0, undefined, 1},		// 0x8D
+    {"ADC A,(HL)", 0, undefined, 2},	// 0x8E
+    {"ADC A,A", 0, undefined, 1},		// 0x8F
+    {"SUB B", 0, undefined, 1},			// 0x90
+    {"SUB C", 0, undefined, 1},			// 0x91
+    {"SUB D", 0, undefined, 1},			// 0x92
+    {"SUB E", 0, undefined, 1},			// 0x93
+    {"SUB H", 0, undefined, 1},			// 0x94
+    {"SUB L", 0, undefined, 1},			// 0x95
+    {"SUB (HL)", 0, undefined, 2},		// 0x96
+    {"SUB A", 0, undefined, 1},			// 0x97
+    {"SBC A,B", 0, undefined, 1},		// 0x98
+    {"SBC A,C", 0, undefined, 1},		// 0x99
+    {"SBC A,D", 0, undefined, 1},		// 0x9A
+    {"SBC A,E", 0, undefined, 1},		// 0x9B
+    {"SBC A,H", 0, undefined, 1},		// 0x9C
+    {"SBC A,L", 0, undefined, 1},		// 0x9D
+    {"SBC A,(HL)", 0, undefined, 2},	// 0x9E
+    {"SBC A,A", 0, undefined, 1},		// 0x9F
+    {"AND B", 0, undefined, 1},			// 0xA0
+    {"AND C", 0, undefined, 1},			// 0xA1
+    {"AND D", 0, undefined, 1},			// 0xA2
+    {"AND E", 0, undefined, 1},			// 0xA3
+    {"AND H", 0, undefined, 1},			// 0xA4
+    {"AND L", 0, undefined, 1},			// 0xA5
+    {"AND (HL)", 0, undefined, 2},		// 0xA6
+    {"AND A", 0, undefined, 1},			// 0xA7
+    {"XOR B", 0, undefined, 1},			// 0xA8
+    {"XOR C", 0, undefined, 1},			// 0xA9
+    {"XOR D", 0, undefined, 1},			// 0xAA
+    {"XOR E", 0, undefined, 1},			// 0xAB
+    {"XOR H", 0, undefined, 1},			// 0xAC
+    {"XOR L", 0, undefined, 1},			// 0xAD
+    {"XOR (HL)", 0, undefined, 2},		// 0xAE
+    {"XOR A", 0, undefined, 1},			// 0xAF
+    {"OR B", 0, undefined, 1},			// 0xB0
+    {"OR C", 0, undefined, 1},			// 0xB1
+    {"OR D", 0, undefined, 1},			// 0xB2
+    {"OR E", 0, undefined, 1},			// 0xB3
+    {"OR H", 0, undefined, 1},			// 0xB4
+    {"OR L", 0, undefined, 1},			// 0xB5
+    {"OR (HL)", 0, undefined, 2},		// 0xB6
+    {"OR A", 0, undefined, 1},			// 0xB7
+    {"CP B", 0, undefined, 1},			// 0xB8
+    {"CP C", 0, undefined, 1},			// 0xB9
+    {"CP D", 0, undefined, 1},			// 0xBA
+    {"CP E", 0, undefined, 1},			// 0xBB
+    {"CP H", 0, undefined, 1},			// 0xBC
+    {"CP L", 0, undefined, 1},			// 0xBD
+    {"CP (HL)", 0, undefined, 2},		// 0xBE
+    {"CP A", 0, undefined, 1},			// 0xBF
+    {"RET NZ", 0, undefined, 5},		// 0xC0 timing 2 or 5
+    {"POP BC", 0, undefined, 3},		// 0xC1
+    {"JP NZ,0x%04x", 2, undefined, 4},	// 0xC2 timing 3 or 4
+    {"JP 0x%04x", 2, undefined, 3},		// 0xC3
+    {"CALL NZ,0x%04x", 2, undefined, 6},// 0xC4 timing 3 or 6
+    {"PUSH BC", 0, undefined, 4},		// 0xC5
+    {"ADD A,0x%02x", 1, undefined, 2},	// 0xC6
+    {"RST 00H", 0, undefined, 4},		// 0xC7
+    {"RET Z", 0, undefined, 5},			// 0xC8 timing 2 or 5
+    {"RET", 0, undefined, 4},			// 0xC9
+    {"JP Z,0x%04x", 2, undefined, 4},	// 0xCA timing 3 or 4
+    {"PREFIX", 0, undefined, 1},		// 0xCB
+    {"CALL Z,0x%04x", 2, undefined, 6},	// 0xCC timing 3 or 6
+    {"CALL 0x%04x", 2, undefined, 6},	// 0xCD
+    {"ADC A,0x%02x", 1, undefined, 2},	// 0xCE
+    {"RST 08H", 0, undefined, 4},		// 0xCF
+    {"RET NC", 0, undefined, 5},		// 0xD0 timing 2 or 5
+    {"POP DE", 0, undefined, 3},		// 0xD1
+    {"JP NC,0x%04x", 2, undefined, 4},	// 0xD2 timing 3 or 4
+    {"undefined", 0, undefined, 0},		// 0xD3
+    {"CALL NC,0x%04x", 2, undefined, 6},// 0xD4 timing 3 or 6
+    {"PUSH DE", 0, undefined, 4},		// 0xD5
+    {"SUB 0x%02x", 1, undefined, 2},	// 0xD6
+    {"RST 10H", 0, undefined, 4},		// 0xD7
+    {"RET C", 0, undefined, 5},			// 0xD8	timing 2 or 5
+    {"RETI", 0, undefined, 4},			// 0xD9
+    {"JP C,0x%04x", 2, undefined, 4},	// 0xDA timing 3 or 4
+    {"undefined", 0, undefined, 0},		// 0xDB
+    {"CALL C,0x%04x", 2, undefined, 6},	// 0xDC timing 3 or 6
+    {"undefined", 0, undefined, 0},		// 0xDD
+    {"SBC A,0x%02x", 1, undefined, 2},	// 0xDE
+    {"RST 18H", 0, undefined, 4},		// 0xDF
+    {"LDH (0x%02x),A", 1, undefined, 3},// 0xE0
+    {"POP HL", 0, undefined, 3},		// 0xE1
+    {"LD (C),A", 0, undefined, 2},		// 0xE2
+    {"undefined", 0, undefined, 0},		// 0xE3
+    {"undefined", 0, undefined, 0},		// 0xE4
+    {"PUSH HL", 0, undefined, 4},		// 0xE5
+    {"AND 0x%02x", 1, undefined, 2},	// 0xE6
+    {"RST 20H", 0, undefined, 4},		// 0xE7
+    {"ADD SP,0x%02x", 1, undefined, 4},	// 0xE8
+    {"JP HL", 0, undefined, 1},			// 0xE9
+    {"LD (0x%04x),A", 2, undefined, 3},	// 0xEA
+    {"undefined", 0, undefined, 0},		// 0xEB
+    {"undefined", 0, undefined, 0},		// 0xEC
+    {"undefined", 0, undefined, 0},		// 0xED
+    {"XOR 0x%02x", 1, undefined, 2},	// 0xEE
+    {"RST 28H", 0, undefined, 2},		// 0xEF
+    {"LDH A,(0x%02x)", 1, undefined, 2},// 0xF0
+    {"POP AF", 0, undefined, 3},		// 0xF1
+    {"LD A,(C)", 0, undefined, 2},		// 0xF2
+    {"DI", 0, undefined, 1},			// 0xF3
+    {"undefined", 0, undefined, 0},		// 0xF4
+    {"PUSH AF", 0, undefined, 4},		// 0xF5
+    {"OR 0x%02x", 1, undefined, 2},		// 0xF6
+    {"RST 30H", 0, undefined, 4},		// 0xF7
+    {"LD HL,SP+0x%02x", 1, undefined, 3},// 0xF8
+    {"LD HL,SP", 0, undefined, 2},		// 0xF9
+    {"LD A,(0x%04x)", 2, undefined, 4},	// 0xFA
+    {"EI", 0, undefined, 1},			// 0xFB
+    {"undefined", 0, undefined, 0},		// 0xFC
+    {"undefined", 0, undefined, 0},		// 0xFD
+    {"CP 0x%02x", 1, undefined, 2},		// 0xFE
+    {"RST 38H", 0, undefined, 4}		// 0xFF
 };
 
 // for any instructions that are not defined
@@ -341,6 +336,29 @@ static void inc_a(void) { registers.a = inc(registers.a); }
 // 0x3D DEC A
 static void dec_a(void) { registers.a = dec(registers.a); }
 
+// 0x3E LD A,n
+static void ld_a_n(uint8_t operand) { registers.a = operand; }
+
+// 0x78 LD A,B
+static void ld_a_b(void) { registers.a = registers.b; }
+
+// 0x79 LD A,C
+static void ld_a_c(void) { registers.a = registers.c; }
+
+// 0x7A LD A,D
+static void ld_a_d(void) { registers.a = registers.d; }
+
+// 0x7B LD A,E
+static void ld_a_e(void) { registers.a = registers.e; }
+
+// 0x7C LD A,H
+static void ld_a_h(void) { registers.a = registers.h; }
+
+// 0x7D LD A,L
+static void ld_a_l(void) { registers.a = registers.l; }
+
+// 0x7E LD A,(HL)
+
 
 ////
 //  B
@@ -355,6 +373,26 @@ static void dec_b(void) { registers.b = dec(registers.b); }
 // 0x06 LD B,n
 static void ld_b_n(uint8_t operand) { registers.b = operand; }
 
+// 0x41 LD B,C
+static void ld_b_c(void) { registers.b = registers.c; }
+
+// 0x42 LD B,D
+static void ld_b_d(void) { registers.b = registers.d; }
+
+// 0x43 LD B,E
+static void ld_b_e(void) { registers.b = registers.e; }
+
+// 0x44 LD B,H
+static void ld_b_h(void) { registers.b = registers.h; }
+
+// 0x45 LD B,L
+static void ld_b_l(void) { registers.b = registers.l; }
+
+// 0x46 LD B,(HL)
+
+// 0x47 LD B,A
+static void ld_b_a(void) { registers.b = registers.a; }
+
 
 ////
 //  C
@@ -368,6 +406,26 @@ static void dec_c(void) { registers.c = dec(registers.c); }
 
 // 0x0E LD C,n
 static void ld_c_n(uint8_t operand) { registers.c = operand; }
+
+// 0x48 LD C,B
+static void ld_c_b(void) { registers.c = registers.b; }
+
+// 0x4A LD C,D
+static void ld_c_d(void) { registers.c = registers.d; }
+
+// 0x4B LD C,E
+static void ld_c_e(void) { registers.c = registers.e; }
+
+// 0x4C LD C,H
+static void ld_c_h(void) { registers.c = registers.h; }
+
+// 0x4D LD C,L
+static void ld_c_l(void) { registers.c = registers.l; }
+
+// 0x4E LD C,(HL)
+
+//0x4F LD, C,A
+static void ld_c_a(void) { registers.c = registers.a; }
 
 
 ////
@@ -397,6 +455,26 @@ static void dec_d(void) { registers.d = dec(registers.d); }
 // 0x16 LD D,n
 static void ld_d_n(uint8_t operand) { registers.d = operand; }
 
+// 0x50 LD D,B
+static void ld_d_b(void) { registers.d = registers.b; }
+
+// 0x51 LD D,C
+static void ld_d_c(void) { registers.d = registers.c; }
+
+// 0x53 LD D,E
+static void ld_d_e(void) { registers.d = registers.e; }
+
+// 0x54 LD D,H
+static void ld_d_h(void) { registers.d = registers.h; }
+
+// 0x55 LD D,L
+static void ld_d_l(void) { registers.d = registers.l; }
+
+// 0x56 LD D,(HL)
+
+// 0x57 LD D,A
+static void ld_d_a(void) { registers.d = registers.a; }
+
 
 ////
 //  E
@@ -410,6 +488,26 @@ static void dec_e(void) { registers.e = dec(registers.e); }
 
 // 0x1E LD E,n
 static void ld_e_n(uint8_t operand) { registers.e = operand; }
+
+// 0x58 LD E,B
+static void ld_e_b(void) { registers.e = registers.b; }
+
+// 0x59 LD E,C
+static void ld_e_c(void) { registers.e = registers.c; }
+
+// 0x5A LD E,D
+static void ld_e_d(void) { registers.e = registers.d; }
+
+// 0x5C LD E,H
+static void ld_e_h(void) { registers.e = registers.h; }
+
+// 0x5D LD E,L
+static void ld_e_l(void) { registers.e = registers.l; }
+
+// 0x5E LD E,(HL)
+
+// 0x5F LD E,A
+static void ld_e_a(void) { registers.e = registers.a; }
 
 
 ////
@@ -439,6 +537,26 @@ static void dec_h(void) { registers.h = dec(registers.h); }
 // 0x26 LD H,n
 static void ld_h_n(uint8_t operand) { registers.h = operand; }
 
+// 0x60 LD H,B
+static void ld_h_b(void) { registers.h = registers.b; }
+
+// 0x61 LD H,C
+static void ld_h_c(void) { registers.h = registers.c; }
+
+// 0x62 LD H,D
+static void ld_h_d(void) { registers.h = registers.d; }
+
+// 0x63 LD H,E
+static void ld_h_e(void) { registers.h = registers.e; }
+
+// 0x65 LD H,L
+static void ld_h_l(void) { registers.h = registers.l; }
+
+// 0x66 LD H,(HL)
+
+// 0x67 LD H,A
+static void ld_h_a(void) { registers.h = registers.a; }
+
 
 ////
 //  L
@@ -452,6 +570,26 @@ static void dec_l(void) { registers.l = dec(registers.l); }
 
 // 0x2E LD L,n
 static void ld_l_n(uint8_t operand) { registers.l = operand; }
+
+// 0x68 LD L,B
+static void ld_l_b(void) { registers.l = registers.b; }
+
+// 0x69 LD L,C
+static void ld_l_c(void) { registers.l = registers.c; }
+
+// 0x6A LD L,D
+static void ld_l_d(void) { registers.l = registers.d; }
+
+// 0x6B LD L,E
+static void ld_l_e(void) { registers.l = registers.e; }
+
+// 0x6C LD L,H
+static void ld_l_h(void) { registers.l = registers.h; }
+
+// 0x6E LD L,(HL)
+
+// 0x6F LD L,A
+static void ld_l_a(void) { registers.l = registers.a; }
 
 
 ////
--- instructions.h
+++ instructions.h
@@ -8,6 +8,16 @@
 
 #include <stdint.h>
 
+// struct containing important information regarding 
+// the processor instructions for GameBoy
+struct instruction 
+{
+    char *disas;        // instruction disassembly
+    uint8_t ops;        // number of operands (0-2)
+    void *exec;  // function to execute instruction
+    uint8_t ticks;      // machine time needed
+};
+
 static void undefined(void);
 
 static uint8_t inc(uint8_t val);
@@ -18,14 +28,36 @@ static void nop(void);
 
 static void inc_a(void);
 static void dec_a(void);
+static void ld_a_n(uint8_t operand);
+static void ld_a_b(void);
+static void ld_a_c(void);
+static void ld_a_d(void);
+static void ld_a_e(void);
+static void ld_a_h(void);
+static void ld_a_l(void);
+//static void ld_a_hla(void);
 
 static void inc_b(void);
 static void dec_b(void);
 static void ld_b_n(uint8_t operand);
+static void ld_b_c(void);
+static void ld_b_d(void);
+static void ld_b_e(void);
+static void ld_b_h(void);
+static void ld_b_l(void);
+//static void ld_b_hla(void);
+static void ld_b_a(void);
 
 static void inc_c(void);
 static void dec_c(void);
 static void ld_c_n(uint8_t operand);
+static void ld_c_b(void);
+static void ld_c_d(void);
+static void ld_c_e(void);
+static void ld_c_h(void);
+static void ld_c_l(void);
+//static void ld_c_hla(void);
+static void ld_c_a(void);
 
 static void ld_bc_nn(uint16_t operand);
 static void inc_bc(void);
@@ -34,10 +66,24 @@ static void dec_bc(void);
 static void inc_d(void);
 static void dec_d(void);
 static void ld_d_n(uint8_t operand);
+static void ld_d_b(void);
+static void ld_d_c(void);
+static void ld_d_e(void);
+static void ld_d_h(void);
+static void ld_d_l(void);
+//static void ld_d_hla(void);
+static void ld_d_a(void);
 
 static void inc_e(void);
 static void dec_e(void);
 static void ld_e_n(uint8_t operand);
+static void ld_e_b(void);
+static void ld_e_c(void);
+static void ld_e_d(void);
+static void ld_e_h(void);
+static void ld_e_l(void);
+//static void ld_e_hla(void);
+static void ld_e_a(void);
 
 static void ld_de_nn(uint16_t operand);
 static void inc_de(void);
@@ -46,10 +92,24 @@ static void dec_de(void);
 static void inc_h(void);
 static void dec_h(void);
 static void ld_h_n(uint8_t operand);
+static void ld_h_b(void);
+static void ld_h_c(void);
+static void ld_h_d(void);
+static void ld_h_e(void);
+static void ld_h_l(void);
+//static void ld_h_hla(void);
+static void ld_h_a(void);
 
 static void inc_l(void);
 static void dec_l(void);
 static void ld_l_n(uint8_t operand);
+static void ld_l_b(void);
+static void ld_l_c(void);
+static void ld_l_d(void);
+static void ld_l_e(void);
+static void ld_l_h(void);
+//static void ld_l_hla(void);
+static void ld_l_a(void);
 
 static void add_hl_bc(void);
 static void add_hl_de(void);
