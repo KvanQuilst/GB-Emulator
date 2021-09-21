83366a9f1ba448de599882997c429f9ed3e243cd
KvanQuilst <eskew.dylan@outlook.com>
2021-09-20 20:24:37 -0700

    memory: added read_byte

--- memory.h
+++ memory.h
@@ -17,3 +17,8 @@ extern uint8_t lram[0x2000];	// low internal ram
 extern uint8_t oam[0xA0];		// sprites
 extern uint8_t io[0x4C];
 extern uint8_t hram[0x7F];		// high internal ram
+
+// read a byte from the memory address space
+// requires: an address for reading from
+// returns: the byte at the address
+uint8_t read_byte(uint8_t address);
