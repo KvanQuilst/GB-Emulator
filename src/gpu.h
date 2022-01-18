decb9bbc504ad906ceee53b01ae53151be72e664
KvanQuilst <eskew.dylan@outlook.com>
2022-01-17 20:20:05 -0800

    finishing up for the night

--- src/gpu.h
+++ src/gpu.h
@@ -11,10 +11,6 @@
 #define HEIGHT 256
 #define WIDTH 256
 #define TILE_NUM 384
-#define GREY0 0xFFFFFF
-#define GREY1 0xa8a8a8
-#define GREY2 0x606060
-#define GREY3 0x000000
 
 extern bool running;
 extern SDL_Window *window;
